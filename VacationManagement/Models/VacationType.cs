ca46afbf4ab97623f0a724321405e713bd159ecf
abu3beid <m76eef@gmail.com>
2022-05-22 23:17:33 +0300

    Added Views and some Models

---
+++ VacationManagement/Models/VacationType.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VacationManagement.Models
+{
+    public class VacationType:EntityBase
+    {
+        [StringLength(200)]
+        [Display(Name ="Vacation Name")]
+        public string VacationName { get; set; } = string.Empty;
+        [Display(Name ="Vacation Color")]
+        [StringLength(7)]
+        public string BackgroundColor { get; set; } = string.Empty;
+        [Display(Name = "Days Number")]
+        public int DaysNumber { get; set; }
+    }
+}
