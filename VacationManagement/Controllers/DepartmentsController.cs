f7e5059c078750b18de7b75fe047395144926e1a
Mohammed AbuIbaid <m76eef@gmail.com>
2022-06-09 15:19:06 +0300

    update

--- VacationManagement/Controllers/DepartmentsController.cs
+++ VacationManagement/Controllers/DepartmentsController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using VacationManagement.Data;
+using VacationManagement.Models;
 
 namespace VacationManagement.Controllers
 {
@@ -7,7 +8,7 @@ namespace VacationManagement.Controllers
     {
         private readonly VacationDbContext _context;
 
-        public DepartmentsController(VacationDbContext _context)
+        public DepartmentsController(VacationDbContext context)
         {
             _context = context;
         }
@@ -19,6 +20,53 @@ namespace VacationManagement.Controllers
         public IActionResult Create()
         {
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Department model)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Departments.Add(model);
+                _context.SaveChanges();
+                return RedirectToAction("Departments");
+            }
+            return View(model);
+        }
+        public IActionResult Edit(int? Id)
+        {
+            return View(_context.Departments.FirstOrDefault(x => x.Id == Id));
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Department model)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Departments.Update(model);
+                _context.SaveChanges();
+                return RedirectToAction("Departments");
+            }
+            return View(model);
+        }
+
+        public IActionResult Delete(int? Id)
+        {
+            return View(_context.Departments.FirstOrDefault(x => x.Id == Id));
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(Department model)
+        {
+            if(model != null)
+            {
+                _context.Departments.Remove(model);
+                _context.SaveChanges();
+                return RedirectToAction("Departments");
+            }
+            return View(model);
         }
     }
 }
