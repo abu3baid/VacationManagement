c36564d056e7789695f84ad3cb76dbf915d3e5ab
Mohammed AbuIbaid <m76eef@gmail.com>
2022-06-09 04:35:59 +0300

    idk

---
+++ VacationManagement/Controllers/DepartmentsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VacationManagement.Data;
+
+namespace VacationManagement.Controllers
+{
+    public class DepartmentsController : Controller
+    {
+        private readonly VacationDbContext _context;
+
+        public DepartmentsController(VacationDbContext _context)
+        {
+            _context = context;
+        }
+        public IActionResult Departments()
+        {
+            return View(_context.Departments.OrderBy(x=>x.Name).ToList());
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
